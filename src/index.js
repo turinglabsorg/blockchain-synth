465d729b36c10554e10f641cb8be4bd9a54bca4a
turinglabsorg <turinglabs@tuta.io>
2019-05-13 18:29:35 +0200

    min changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ var AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var context = new AudioContext();
 var synth = new AudioSynth(context);
-var hex = []
+var txns = []
+var hex
 var isplaying = false
 var timeout = []
 var playingindex = 0
@@ -22,35 +23,37 @@ function playSound(note, byte, time){
     },time)
 }
 
-function playTransaction(index){
+async function playTransaction(index){
     if(isplaying === false){
+        var tx = await axios.get('https://api.blockcypher.com/v1/btc/main/txs/' + txns[index] + '?includeHex=true&limit=1')
+        var toplay = tx.data.hex
         playingindex = index
         isplaying = true
-        document.getElementById("txplaying").innerHTML = 'Playing ' + index + ' TX';
-        var toplay = hex[index]
+        document.getElementById("txplaying").innerHTML = 'Playing TX #' + playingindex;
         document.getElementById("rawtx").innerHTML = toplay;
 
         synth.setOscWave(0); 
         synth.setDelayFeedback(0.5); 
         synth.setDelayTimeTempo(110, 0.25);
         
-        for(var i=0; i < toplay.length; i+=2){
+        for(var i=0; i <= toplay.length; i+=2){
             var note = parseInt(toplay.substr(i, 2), 16)
             console.log(note)
             var byte = toplay.substr(i, 2)
             var time = i * 100
-            playSound(note, byte, time);
+            if( i < toplay.length){
+                playSound(note, byte, time);
+            } else {
+                playNext()
+            }
         }
     }
 }
 
 function playNext(){
-    timeout.forEach(function(value, index){
-        clearTimeout(timeout[index])
-    })
     isplaying = false
     var playnext = playingindex++
-    if(raw[playnext] !== undefined){
+    if(txns[playnext] !== undefined){
         playTransaction(playNext)
     }else{
         playTransaction(0)
@@ -60,19 +63,13 @@ function playNext(){
 async function parselastblock(){
     var blockchain = await axios.get('https://api.blockcypher.com/v1/btc/main')
     var lastblock = blockchain.data
-    await sleep(2000)
     var block = await axios.get('https://api.blockcypher.com/v1/btc/main/blocks/' + lastblock.hash)
     var blockdata = block.data
     await sleep(2000)
     for(var i = 0; i < blockdata.txids.length; i++){
-        var tx = await axios.get('https://api.blockcypher.com/v1/btc/main/txs/' + blockdata.txids[i] + '?includeHex=true&limit=1')
-        var txhex = tx.data.hex
-        hex.push(txhex)
-        await sleep(4000)
-        document.getElementById("nextbtn").style.display = block 
-        playTransaction(i)
+        txns.push(blockdata.txids[i])
     }
+    playTransaction(0)
 }
 
 parselastblock()
-document.getElementById("nextbtn").addEventListener("click", playNext);
